da57c5ea804733a01923ec9e24982aa920bd0866
JOLLY <evifere@gmail.com>
2020-04-20 14:46:23 +0200

    add koa router

--- mailserver/server.js
+++ mailserver/server.js
@@ -1,11 +1,15 @@
-
 'use strict';
-
 const koa = require('koa')
+const koaRouter = require('koa-router')
+
 const app = new koa()
+const router = new koaRouter()
 
-app.use(function *(){
-  this.body = "Hello World !!!";
-});
+router.get('koala', '/', (ctx) => {
+  ctx.body = "Welcome! To the Koala Book of Everything!"
+})
 
-app.listen(1234)
+app.use(router.routes())
+  .use(router.allowedMethods())
+
+app.listen(1234, () => console.log('running on port 1234'))
