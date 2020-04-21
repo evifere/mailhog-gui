f926b24a4a7d5107f60c874dc64a8d0e88452b78
JOLLY <evifere@gmail.com>
2020-04-21 15:08:15 +0200

    add visibility link

--- mailapp/src/components/MessageRow.jsx
+++ mailapp/src/components/MessageRow.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
+import { Link } from "react-router-dom";
+import IconButton from '@material-ui/core/IconButton';
+import Visibility from '@material-ui/icons/Visibility';
 
-export default class MessageRow extends React.Component {
-  state = {
-    message: {}
-  }
+const liStyle = {
+    display: 'table-row',
+};
 
-  render() {
-    const {message} = this.props; 
+export default function MessageRow({ message = {} }) {
+    const btnDetail = <IconButton> <Link to={"/message/" + message.ID}><Visibility /></Link></IconButton>;
+
     return (
-      <li>{message.ID}</li>
+        <li style={liStyle} key={message.ID}>{btnDetail}&nbsp;From {message.Content.Headers.From.map(from => <span>{from}</span>)} | {message.Content.Headers.Subject[0]} &nbsp;|
+        </li>
     )
-  }
+
 }
