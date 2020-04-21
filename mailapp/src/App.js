10eedfc92b90844b55fe1b6fd58c7c8f792c619b
JOLLY <evifere@gmail.com>
2020-04-21 14:13:58 +0200

    define app class

--- mailapp/src/App.js
+++ mailapp/src/App.js
@@ -16,52 +16,56 @@ import PrimarySearchAppBar from './components/PrimarySearchAppBar'
 // making sure things like the back button and bookmarks
 // work properly.
 
-export default function BasicExample() {
-  return (
-    <Router>
-      <PrimarySearchAppBar mailCount="2"/>
-      <div>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/dashboard">Dashboard</Link>
-          </li>
-          <li>
-            <Link to="/all">All</Link>
-          </li>
-        </ul>
+export default class App extends React.Component {
 
-        <hr />
+  render() {
 
-        {/*
+    return (
+      <Router>
+        <PrimarySearchAppBar mailCount="2" />
+        <div>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/dashboard">Dashboard</Link>
+            </li>
+            <li>
+              <Link to="/all">All</Link>
+            </li>
+          </ul>
+
+          <hr />
+
+          {/*
           A <Switch> looks through all its children <Route>
           elements and renders the first one whose path
           matches the current URL. Use a <Switch> any time
           you have multiple routes, but you want only one
           of them to render at a time
         */}
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/dashboard">
-            <Dashboard />
-          </Route>
-          <Route path="/all">
-            <AllMessagesList />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
-  );
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/dashboard">
+              <Dashboard />
+            </Route>
+            <Route path="/all">
+              <AllMessagesList />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
 }
 
 // You can think of these components as "pages"
