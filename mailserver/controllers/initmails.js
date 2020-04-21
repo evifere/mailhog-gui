9bcef5f7a5f0fd0208084a9316be731cd3435001
JOLLY <evifere@gmail.com>
2020-04-21 11:41:59 +0200

    setup sendmail

--- mailserver/controllers/initmails.js
+++ mailserver/controllers/initmails.js
@@ -1,8 +1,33 @@
 
-module.exports = function(router) {
+const sendmail = require('sendmail')({
+    devPort: 1025, // Default: False
+    devHost: 'localhost', // Default: localhost
+    smtpHost: 'localhost',
+    smtpPort: 1025
+})
+
+module.exports = function (router) {
     router.get('initmails', '/initmails', (ctx) => {
-        ctx.body = {"message":"init mails done"};
-      })
-  };
+        sendmail({
+            logger: {
+                debug: console.log,
+                info: console.info,
+                warn: console.warn,
+                error: console.error
+            },
+            silent: false,
+            from: 'test@mailhog.api.local',
+            to: 'info@mailhog.api.local',
+            /*  replyTo: 'jason@mailhog.api.local',*/
+            subject: 'MailComposer sendmail',
+            html: 'Mail of test sendmail '
+        }, function (err, reply) {
+            console.log('errors :', err && err.stack)
+            console.dir('reply = ', reply)
+            ctx.body = { "message": "init mails done" };
+        })
+
+    })
+};
 
 
